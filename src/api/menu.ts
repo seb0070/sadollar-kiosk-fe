74b0894d1112f4986b32e9e8a891b3f4a6906255
jsb <tpqls1703@gmail.com>
2026-04-27 00:30:30 +0900

    feat(api): 메뉴 API 함수 추가 (#2)

---
+++ src/api/menu.ts
@@ -0,0 +1,12 @@
+import client from './client';
+import { MenuItem } from '../types';
+
+export const getMenus = async (q?: string): Promise<MenuItem[]> => {
+  const res = await client.get('/menu', { params: q ? { q } : {} });
+  return res.data.items;
+};
+
+export const getMenuById = async (id: number): Promise<MenuItem> => {
+  const res = await client.get(`/menu/${id}`);
+  return res.data;
+};
