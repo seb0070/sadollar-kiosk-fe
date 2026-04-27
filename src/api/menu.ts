a43970a4bb95c0a0afc3ce669f79ab68f5116c54
jsb <tpqls1703@gmail.com>
2026-04-27 09:22:33 +0900

    fix: 메뉴 전체 조회 limit 200으로 수정 (#3)

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -2,8 +2,9 @@ import client from './client';
 import type { MenuItem } from '../types';
 
 export const getMenus = async (q?: string): Promise<MenuItem[]> => {
-  const res = await client.get('/menu', { params: q ? { q } : undefined });
-  console.log('메뉴 응답:', res.data);
+  const res = await client.get('/menu', {
+    params: { limit: 200, ...(q ? { q } : undefined) } 
+  });
   return res.data.items;
 };
 
